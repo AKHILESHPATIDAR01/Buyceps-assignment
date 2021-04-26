7e2a59fe54a606c4309c66b60273f7577f7dd30f
AKHILESHPATIDAR01 <akhilesh.forsignin@gmail.com>
2021-04-26 19:05:31 +0530

    first commit

--- components/SearchBar/index.jsx
+++ components/SearchBar/index.jsx
@@ -1,6 +1,24 @@
 import React, { useEffect, useState } from "react";
 
-export default function SearchBar(props) {
+export default function SearchBar( { setSerachTitle , setsearchYear ,setSearchId  } ) {
+	const [titleInput, setTitleInput]=useState("");
+	const [yearInput, setYearInput]=useState("");
+	const [idInput, setIdInput]=useState("");
+
+	const handleSubmit = (e)=>{
+		e.preventDefault();
+		// console.log("SUBMIT CLICKED");
+		if(titleInput !== ""){
+			setSerachTitle(titleInput);
+			setSearchId(idInput);
+			setsearchYear(yearInput);
+		}
+		else{
+			
+		}
+		
+		console.log(titleInput , yearInput , idInput  );
+	}
 
 	return (
 		<div className="relative shadow-lg my-10 p-5 rounded-md mx-5">
@@ -15,6 +33,7 @@ export default function SearchBar(props) {
 						className="py-2 text-sm col-span-2 md:col-auto text-gray-600 bg-gray-100 rounded-md px-2  focus:outline-none focus:ring focus:border-purple-600 focus:bg-white focus:text-gray-900"
 						placeholder="Search Title"
 						autoComplete="off"
+						onChange={ e => setTitleInput(e.target.value) }
 					/>
 					<input
 						type="search"
@@ -22,6 +41,7 @@ export default function SearchBar(props) {
 						className="py-2 text-sm text-gray-600 bg-gray-100 rounded-md px-2  focus:outline-none focus:ring focus:border-purple-600 focus:bg-white focus:text-gray-900"
 						placeholder="Year"
 						autoComplete="off"
+						onChange={ e => setYearInput(e.target.value) }
 					/>
 					<input
 						type="search"
@@ -29,10 +49,12 @@ export default function SearchBar(props) {
 						className="py-2 text-sm  text-gray-600 bg-gray-100 rounded-md px-2  focus:outline-none focus:ring focus:border-purple-600 focus:bg-white focus:text-gray-900"
 						placeholder="ID"
 						autoComplete="off"
+						onChange={ e => setIdInput(e.target.value) }
 					/>
 					<button
 						type="submit"
-						className=" search-btn text-white font-bold p-1 rounded col-span-2 md:col-auto bg-yellow-400 focus:outline-none focus:shadow-outline">
+						className=" search-btn text-white font-bold p-1 rounded col-span-2 md:col-auto bg-yellow-400 focus:outline-none focus:shadow-outline"
+						onClick={handleSubmit}  >
 						Search
 					</button>
 				</div>
--- components/SearchPage/index.jsx
+++ components/SearchPage/index.jsx
@@ -1,18 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import { BrowserRouter , Switch , Route } from 'react-router-dom';
 import Data from '../../public/sampleData.json'
 import { data } from "autoprefixer";
 const SearchBar = dynamic(() => import("../SearchBar"));
 
 export default function SearchPage(props) {
-  console.log(Data.Search)
+	const [displayData, setDisplayData] = useState([]);
+	const [pageNumber , setPageNumber] = useState(1);
+	const [serachTitle , setSerachTitle] = useState("Radhe");
+	const [searchYear , setsearchYear] = useState(2021);
+	const [searchId , setSearchId] = useState("tt10888594");
+
+	// console.log(Data.Search);
+
+
+	const fetchData = async(pageNumber) =>{
+		// const result = await fetch(`http://www.omdbapi.com/?apikey=b29ea5a1&t={title}&y={year}&i={id}`)
+		const result = await fetch(`http://www.omdbapi.com/?apikey=b29ea5a1&s=${serachTitle}&y=${searchYear}&i=${searchId}&plot=full`);
+		const newData = await result.json();
+		console.log("fetched Data :",newData.Search);
+		setDisplayData(newData.Search);
+	}
+
+	useEffect(()=>{
+		fetchData(pageNumber);
+		console.log("Search Title :",serachTitle);
+	},[pageNumber , serachTitle ,searchId ,searchYear]);
+
 	return (
-		<>
+		<>  
 			<p className="text-center my-10 text-3xl font-medium">Explore movies</p>
 			<div className="boxed">
-				<SearchBar />
-        {/* Display Search results here */}
+				<SearchBar setSerachTitle={setSerachTitle}  setsearchYear={setsearchYear} setSearchId={setSearchId}  />
+        		{/* Display Search results here */}
+				
+				<div className="display-items">
+					{displayData.map((item)=>{
+						return(
+							<div className='display-card'>
+								<div className='display-img-portion'>
+									<img src={item.Poster} />
+								</div>
+								<div className='display-detail-portion'>
+									<h1 className="display-title">
+										{ item.Title.length <20 ? item.Title : null }
+									</h1>
+									<h1 className="display-heading">Directed by :<br></br><span>{item.Year}</span></h1>
+									<h1 className="display-heading">IMDB : <br></br><span>{item.imdbID}</span></h1>
+									<div className='display-lastline'>
+										<h1 className="display-heading">Type : <br></br><span>{item.Type}</span></h1>
+										<a href="" className="morebtn">more</a>
+									</div>
+									
+								</div>
+								<div className="display-card-hover">
+									{item.Title}
+								</div>
+							</div>
+						);
+					})}
+				</div>
+
+				
+
+				{/* { displayData.length == 0 ?  <p>Please fill correct value</p>
+				:
+					displayData.map((item)=>{
+						return(
+							<h1>{item.Title}</h1>
+						);
+					})
+				} */}
+
+
 			</div>
+			
 		</>
 	);
 }
